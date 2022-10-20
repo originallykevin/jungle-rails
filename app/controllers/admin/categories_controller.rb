9766709afef33edd8fa495755171f58ee2f98a67
originallykevin <ly1@ualberta.ca>
2022-10-20 21:26:23 +0000

    update category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,17 +11,22 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-
     if @category.save
-      redirect_to [:admin, :products], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
+   
   end
-
   def destroy
     @category = Category.find params[:id]
     @category.destroy
     redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
