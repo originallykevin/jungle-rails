6660073ba18cf50ea2bf27df004ef1e4c17bdb92
originallykevin <ly1@ualberta.ca>
2022-10-21 07:37:30 +0000

    refactor sale status

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,14 @@
 class Sale < ApplicationRecord
+
+  def finished?
+    self.ends_on < Date.current
+  end
+
+  def upcoming?
+    self.starts_date > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end
