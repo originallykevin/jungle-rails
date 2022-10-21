f875cae2af8578d036c6a18febefdafd4aef9fc7
originallykevin <ly1@ualberta.ca>
2022-10-21 07:58:28 +0000

    add sales alert on front page and query if it is active

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,11 @@
 class Sale < ApplicationRecord
 
+  # AR scope
+  def self.active
+    where("sales.starts_date <= ? AND sales.ends_on >= ?",
+      Date.current, Date.current)
+  end
+
   def finished?
     self.ends_on < Date.current
   end
