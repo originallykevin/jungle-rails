6da4994f9a0f57e632991c231bc091e5b0a5342b
originallykevin <ly1@ualberta.ca>
2022-10-24 23:26:56 +0000

    init rspec test for product

---
+++ spec/models/product_spec_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    #ensures that a product with all four fields set will indeed save successfully
+    it 'product with all four fields set will be save' do
+      
+    end
+    
+    # validates :name, presence: true
+    it 'validates name' do
+      
+    end
+
+    # validates :price, presence: true
+    it 'validates price' do
+      
+    end
+
+    # validates :quantity, presence: true
+    it 'validates quantity' do
+      
+    end
+
+    # validates :category, presence: true
+    it 'validates category' do
+      
+    end
+
+  end
+end
+
