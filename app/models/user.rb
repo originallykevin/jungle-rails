b76158a652950f5675117a236ecaa46a5fbfa4a2
originallykevin <ly1@ualberta.ca>
2022-10-25 03:50:15 +0000

    add email uniqueness of case sensitivity on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, confirmation: { case_sensitive: false }, uniqueness: true, on: :create
-
   validates :name, presence: true
   validates :password_digest, length: { minimum: 4 , 
     too_short: "is too short (mimimum is %{count} characters)" }
-  validates :email, presence:true, confirmation: { case_sensitive: false }
+  validates :email, presence:true, uniqueness: { case_sensitive: false }, on: :create
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     # password and password_confirmation must match
     it 'validates input of password is same as password confirmation' do
-      user = User.create(name:'Nori Cet', password:'password', password_confirmation:'notpassword')
+      user = User.create(name:'Nori Cet', email:'nori@cet.com', password:'password', password_confirmation:'notpassword')
+      user.errors.full_messages
     expect(user.errors.full_messages).to include(/Password/)
+    end
+
+    # email must be unique 
+    it 'validates email uniqueness for case sensitivity' do
+      user1 = User.create(name:'Nori Cat', email: 'test@test.COM', password:'password', password_confirmation:'password')
+      user1.save
+      user2 = User.create(name:'Nori Cat', email: 'TEST@TEST.com', password:'password', password_confirmation:'password')
+      user2.save
+    expect(user2.errors.full_messages).to include(/Email/)
     end
   end
 
